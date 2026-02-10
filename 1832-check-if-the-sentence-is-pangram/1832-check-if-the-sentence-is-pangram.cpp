bd13308ec20133ba188c7644aae35385f97544ad
Kollu swamy prasad <152318008+swamyprasad9525@users.noreply.github.com>
2026-02-10 19:45:15 +0530

    Time: 3 ms (9.09%), Space: 8.8 MB (60.44%) - LeetHub

--- 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.cpp
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.cpp
@@ -1,16 +1,32 @@
 class Solution {
 public:
     bool checkIfPangram(string sentence) {
+        set <char> s;
+        for(char i='a';i<='z';i++){
+            s.insert(i);
+        }
+        for(char i:sentence){
+            s.erase(i);
+        }
+        if(s.empty()) return 1;
+        return 0;
 
-        vector <int> freq(26,0);
-        for(char c:sentence){
-            freq[c-97]++; //97 is the ascii value of "a" 
-        }
-        for(int i:freq){
-            if(i==0){
-                return 0;
-            }
-        }
-        return 1;
     }
 };
+
+// class Solution {
+// public:
+//     bool checkIfPangram(string sentence) {
+
+//         vector <int> freq(26,0);
+//         for(char c:sentence){
+//             freq[c-97]++; //97 is the ascii value of "a" 
+//         }
+//         for(int i:freq){
+//             if(i==0){
+//                 return 0;
+//             }
+//         }
+//         return 1;
+//     }
+// };
